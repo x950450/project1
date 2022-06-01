a25d90eeb042acf9b0b0ba2dd9322673342a63f8
x950450 <68738260+x950450@users.noreply.github.com>
2022-06-01 18:07:59 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "rg1-name" {
+    type = string
+
+    #default = "RG-Group-01"
+
+}
+
+
+
+variable "location" {
+    type = string
+    description = "Azure location of terraform server environment"
+    #default = "westus2"
+
+}
