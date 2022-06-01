e99a3fa0c78164a548979efaa236abb3cc72f23e
x950450 <68738260+x950450@users.noreply.github.com>
2022-06-01 18:05:43 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+
+  features {}
+
+  subscription_id = "3e958f5d-ca02-4a7c-802a-df9ca714b90b"
+  client_id       = "3ca7ff64-e5b4-4cec-bc86-c3699e242ce3"
+  client_secret   = "GIm8Q~.zkV.1SBcVzwttQW4v3mfxlo-pbcVFUckf"
+  tenant_id       = "e7f6afcc-7dd9-4bf7-991d-c8c64fcc1bb0"
+}
